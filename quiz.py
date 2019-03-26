625371807641946daaa85daddd5d90831eea7cbb
Friend o' the Fluffy <34939082+mewhubHawk@users.noreply.github.com>
2019-03-26 18:40:48 +0000

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,34 @@
+score = 0
+print("Hallo Milla :D")
+print("translate from german to english and vice versa.")
+print("PLEASE NOTE THIS IS CASE SENSITIVE!")
+print("THIS IS MADE FOR GERMAN PEOPLE TO LEARN ENGLISH AND VICE VERSA DON'T CHEAT!")
+questions=\
+  {
+    "müde(mooder)":"tired",
+    "you are(informal)":"du bist",
+    "glücklich":"happy",
+    "she is(informal)":"sie ist",
+    "hello":"hallo",
+    "Katze":"cat",
+    "es ist(informal)":"it is",
+    "Freund":"friend",
+    "blue":"Blau",
+    "New Zealand":"Neuseeland"
+    #add more Qs here
+}
+
+for question, answer in questions.items():
+    response = input("What is '%s' translated to?\n--->" % question)
+    if response != answer:
+        print("No, '%s' is  translated to '%s'." % (question, answer))
+    else:
+        print("Yes, that's correct!")
+        score += 1
+
+print("You got %d out of %d!" % (score, len(questions)))
+
+if score < len(questions)/3:
+   print("better luck next time")
+else:
+ print("Well Done!")
